df0343c6868354689fa8e2c3b9472777465d7eab
teragoon <teragoon@naver.com>
2012-04-12 16:22:48 +0900

    prototype join/get

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require('express')
 
 var app = express.createServer()
 	, socket = io.listen(app)
-	, serverNames = {};
+	, servers = {}
+	, server_idx = {};
 
 socket.configure(function(){
 	socket.set('log level', 4);
@@ -14,6 +15,7 @@ socket.configure(function(){
 app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
+	app.set('view options', { layout: false });
 	app.use(express.bodyParser());
 	app.use(express.favicon());
 	app.use(express.logger({ buffer: 5000}));
@@ -32,34 +34,93 @@ app.get('/', function (req, res) {
 socket.on('connection', function(con){
 	console.log("connection : " + con.id);
 	
-	con.on('message', function(msg){
-		console.log('con message ->');
-		console.dir(msg);		
+	con.on('getServerByRR', function(key, fn){
+		console.log('con getServerByRR -> ');
+		console.dir(key);
+
+		fn(getServerByRR(key));
+		
 	});
 	
-	con.on('serverName', function(msg){
-		var serverName = msg.serverName;
-		
-		if(serverNames[serverName]){
-			console.log('serverName-> ' + serverName + ' : ture');
-		} else{
-			console.log('serverName-> ' + serverName + ' : false');
-			serverNames[serverName] = con.serverName = serverName;
-			con.broadcast.emit('announcement', {server: serverName, action: 'connected'});
-			con.broadcast.emit('serverNames', serverNames);
+	con.on('getServerListByRR', function(fn){
+		console.log('con getServerListByRR -> ');
+		var i = 0;
+		var result = [];
+		for(key in servers){
+			result.push(getServerByRR(key));
 		}
-		
-	})
+		console.log('result -> ');
+		console.dir(result);
+				
+		fn(result);
+	});
+	
+	con.on('join', function(msg){
+		console.log('new join ->');
+		console.dir(msg);
+		for(key in msg){
+			if(servers[key]){
+				servers[key].push(msg[key]);
+			}else{
+				servers[key] = [];
+				servers[key].push(msg[key]);
+			}
+			console.log('length : ' + servers[key].length);
+			//server_idx[key] = servers[key].length;
+			con.serverName = msg[key].serverName;
+		}
+		console.log('servers - >');
+		console.dir(servers);
+//		console.log('server_idx - >');
+//		console.dir(server_idx);
+		console.log('connect - > ' + con.serverName);
+	});
 	
 	con.on('disconnect', function(){
 		if(!con.serverName) return;
 		
-		delete serverNames[con.serverName];
-		con.broadcast.emit('announcement', {server: socket.serverName, action: 'disconnected'})
-		con.broadcast.emit('serverNames', serverNames);
-		console.log("disconnect : " + con.id);
+		for(key in servers){
+			for(var i=0; i < servers[key].length; i++){
+				console.log('-> ' + servers[key][i].serverName);
+				
+				if(servers[key][i].serverName == con.serverName){
+					console.log('del -> ' + servers[key][i]);
+					servers[key].splice(i, 1);
+//					server_idx[key] = servers[key].length;
+				}
+			}
+		}
+		
+		console.log('servers - >');
+		console.dir(servers);
+//		console.log('server_idx - >');
+//		console.dir(server_idx);
+		console.log("disconnect : " + con.serverName);
+		
+		con.broadcast.emit('announcement', {server: con.serverName, action: "disconnected"});
 	})
 });
+
+function getServerByRR(key){
+	var i = 0;
+	if(server_idx[key]){
+		console.log('-> ' + server_idx[key]);
+		i = server_idx[key];
+		if(i >= servers[key].length){
+			i = 0;
+		}
+	}else{
+		console.log('-> ' + server_idx[key]);
+		server_idx[key] = 0;
+		i=0;
+	}
+	console.log('return -> ');
+	console.dir(servers[key][i]);
+	server_idx[key] = i + 1;
+	console.log('idx -> ');
+	console.dir(server_idx);
+	return servers[key][i];
+}
     
 app.listen(8000);
 
